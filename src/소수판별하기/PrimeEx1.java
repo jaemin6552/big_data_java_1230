a9c2ec3305c90273a8ee68075a7c3a3882bf4707
KJM <jaemin6552@naver.com>
2023-01-11 15:09:57 +0900

    1/11

--- src/소수판별하기/PrimeEx1.java
+++ src/소수판별하기/PrimeEx1.java
@@ -16,9 +16,13 @@ public class PrimeEx1 {
 
     static boolean isPrime(int n){ //static이붙으면 객체소속이아닌 메소드가됨
         //1과 입력 받은 값을 제외, 즉 2 ~ 입력 받은 값 미만
-        for(int i = 2; i < n; i++){
-            if(n % i ==0 ) return false; //전달받은 수를 1과 자신을 제외하고 나누어지는 경우를 의미
+        for(int i = 2; i < n; i++) {
+            if ((n % i) == 0) {
+                return false; //전달받은 수를 1과 자신을 제외하고 나누어지는 경우를 의미
+            }
         }
+        if(n ==1) return false;
+
         return true;
     }
 }
