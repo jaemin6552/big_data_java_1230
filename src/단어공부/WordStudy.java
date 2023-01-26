0e22bee5f94c71a4c90cb865d8bf74514a757ccb
JM <jaemin6552@naver.com>
2023-01-26 21:44:05 +0900

    1-26집에서한거

--- src/단어공부/WordStudy.java
+++ src/단어공부/WordStudy.java
@@ -12,16 +12,31 @@ import java.util.Scanner;
 public class WordStudy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        String input1 = input.toUpperCase();
-        int count = 0;
-        char []tmp = new char[input.length()];
+        System.out.print("알파벳  입력 : ");
+        String alphabet = sc.next();
+        alphabet = alphabet.toUpperCase();
+        int []abcCount = new int[26];
 
-        for(int i = 0; i<input1.length(); i++){
-            if(input1.charAt(i) >= 'A' && input1.charAt(i) <='Z'){
-                //char[count++] = input1.charAt(i);
+        for(int i = 0; i<alphabet.length(); i++) {
+            for(int j = 0; j<abcCount.length; j++){
+                if(alphabet.charAt(i) == 'A'+ j) {
+                    abcCount[j]++;
+                }
             }
         }
-        System.out.println(Arrays.toString(tmp));
+
+        int mostCount = abcCount[0];
+        int mostAlpa = 0;
+        for(int i = 0; i<abcCount.length; i++){
+            if(abcCount[i] > mostCount) {
+                mostCount = abcCount[i];
+                mostAlpa = i;
+            } else if(abcCount[i] == mostCount){
+                if(mostAlpa != i) mostAlpa = -2;
+                else mostAlpa = i;
+            }
+
+        }
+        System.out.println("가장 많이나온 알파벳 : " + (char)(mostAlpa+'A'));
     }
 }
