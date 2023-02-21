648eb09c85478a11f2c9cd5bdfea17ae3d3066c2
KJM <jaemin6552@naver.com>
2023-02-21 15:12:51 +0900

    2/21알고리즘

--- src/테스트/test.java
+++ src/테스트/test.java
@@ -6,8 +6,8 @@ import java.util.*;
 class test {
 
     public static void main(String[] args) {
-        int []array = {1,5,2,6,3,7,4};
-        int [][]commands ={{2,5,3},{4,4,1},{1,7,3}};
+        int []array = {1,5,2,6,3,7,4}; //1차배열주어짐
+        int [][]commands ={{2,5,3},{4,4,1},{1,7,3}}; //0,0 시작 0,1끝 0,3 추출해야할인덱스번호
         int k = 0;
         List<Integer> answer = new ArrayList<>();
         for(int i =0; i<commands.length; i++){
